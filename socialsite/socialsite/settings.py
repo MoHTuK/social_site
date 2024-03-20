f9dbb4dffde77e74fbd43bed9decee60f8482c9a
Alexandr <sasha.v720@gmail.com>
2024-03-20 15:27:01 +0200

    create new app 'image', model,form,view for bookmark img

--- socialsite/socialsite/settings.py
+++ socialsite/socialsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'images.apps.ImagesConfig',
     'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
